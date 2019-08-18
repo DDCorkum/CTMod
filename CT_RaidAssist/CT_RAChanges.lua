b5f10c5c6502794ff655976f9567aa0c4008e1c6
Dahk Celes <40327147+DDCorkum@users.noreply.github.com>
2019-08-17 20:18:55 -0600

    8.2.0.5b3
    All-in on the CTRA rewrite now that it is maturing.

--- CT_RaidAssist/CT_RAChanges.lua
+++
@@ -1,7 +0,0 @@
-------------------------------------------------
--- CONTENT REMOVED IN 8.1.5.2
--- This wasn't being maintained since 5.3 anyways
--- and is now integrated into CT_RASlashCmds.lua
--- with a much simpler, less bloated solution
-------------------------------------------------
- 
